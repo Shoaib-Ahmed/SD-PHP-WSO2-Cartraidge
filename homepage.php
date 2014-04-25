0d5e34fba463070b7eff25e518bc485281829904
Shoaib <sahmed25@asu.edu>
2014-04-25 00:51:15 -0700

    Create homepage.php

---
+++ homepage.php
@@ -0,0 +1,38 @@
+<!DOCTYPE HTML> 
+<html>
+	<head>
+		<style>
+			.error {color: #FF0000;}
+		</style>
+		<title>Software Design</title>
+	</head>
+<body>
+<h2 align="center">Software Design Trial Application</h2>
+<table border='2'; align="center">
+	<tr>
+		<td>Group 14</td>
+		<td>Akshay Vaid</td>
+		<td>Shoaib Ahmed</td>
+		<td>Mohan Thorat</td>		
+	</tr>
+	<tr>
+		<td>Group 8</td>
+		<td>Eric Beerman</td>
+		<td>Phillipe Faucon</td>
+		<td>Joseph Millazo</td>	
+	</tr>
+	<tr>
+		<td>Group 27</td>
+		<td>Chaitanya Deshmukh</td>
+		<td>Saumil Hukerikar</td>
+		<td>Roshan Chaudhari</td>	
+	</tr>
+</table>
+	<?php
+		echo 'Sleeping for 5 seconds and moving to next page';
+		sleep(1);
+		print "<META http-equiv='refresh' content='3;URL=benchmark.php'>";
+		exit;
+	?>
+</body>
+</html>
